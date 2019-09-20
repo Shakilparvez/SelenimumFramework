796ebec07dd161366903908b41d1343614b1bebf
shakilparvez <shakil.parvez82@gmail.com>
2019-09-19 21:32:13 -0400

    SearchMusic
    Amazon Music Search test1

--- src/base/Config.java
+++ src/base/Config.java
@@ -1,5 +1,38 @@
 package base;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class Config {
 
+	public WebDriver driver;
+	
+	@BeforeMethod
+	
+	public void beforeEachMethod() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		driver.manage().window().maximize();	
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	
+	}
+	
+	@AfterMethod
+	
+	public void afterEachMethod() {
+		
+		driver.quit();
+	}
 }
+	
+	
+	
+	
+
--- src/test/Amazon_Test1.java
+++ src/test/Amazon_Test1.java
@@ -1,5 +1,44 @@
 package test;
 
-public class Amazon_Test1 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import base.Config;
+
+public class Amazon_Test1  extends Config {
+
+	// in this class we are running each test with the @Test - TestNG Annotation
+	
+	@Test(priority = 1) // this test will execute first because of the priority
+	
+	public void amazonSearchMusic() {
+	System.out.println("This is Amazon Music search test");
+	String expected = "music";
+	//locate the search box and type music.
+	driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys(expected);
+	//click on the search button
+	driver.findElement(By.xpath("//*[@id=\"nav-search\"]/form/div[2]/div/input")).click();
+	// assert if the "music" is in the search result
+	
+	WebElement exp = driver.findElement(By.xpath("//*[@id=\"search\"]/span[2]/h1/div/div[1]/div/div/span[3]"));
+	//store the actual search result in a String variable 
+	String actual = exp.getText();
+	//getting rid of the "" from the actual search result
+	String actualvalue = actual.replaceAll("\"","");
+	Assert.assertEquals(actualvalue, expected);
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
 }
